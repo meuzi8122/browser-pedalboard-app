0a606435ce9952d2379de8664bcfaeae035918f0
meuzi8122 <kagiakete1234@gmail.com>
2024-10-20 19:02:50 +0900

    tone.js動作確認

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+  import * as Tone from "tone";
+
+  async function onClick() {
+    const player = new Tone.Player("sample.wav").toDestination();
+    const reverb = new Tone.Distortion(0.3);
+    player.connect(reverb.toDestination());
+    player.connect(new Tone.Delay(0.5).toDestination());
+    await Tone.loaded();
+    player.start();
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={onClick}>Play</button>
